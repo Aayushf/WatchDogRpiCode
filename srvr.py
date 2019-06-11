fd6bb332d56113abe3a23b1dd8d056f0578b600d
Aayush Fadia <aayushf@gmail.com>
2019-06-11 11:08:41 +0000

    Save wifi networks to wpa_supplicant according to queries from android client.

--- srvr.py
+++ srvr.py
@@ -1,5 +1,26 @@
 from http import server
 from wifi import Cell, Scheme
+def save_wifi(path):
+    qstring = path.split('?')[1]
+    print(qstring)
+    qs = qstring.split('&')
+    print(qs)
+    qdict = {}
+    for q in qs:
+        w = q.split('=')
+        qdict[w[0]] = w[1]
+    print(qdict)
+    block = ("""
+network={
+        ssid="%s"
+        psk="%s"
+        key_mgmt=WPA-PSK
+}""" %(qdict['ssid'], qdict['passwd']))
+    print(block)
+    with open('/etc/wpa_supplicant/wpa_supplicant-wlan0.conf', 'a') as f:
+        f.write(block)
+        f.close()
+
 n = 'yo'
 nets = list(Cell.all('wlan0'))
 print(nets)
@@ -14,10 +35,13 @@ class Handler(server.BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
+        print(self.path)
         pth = self.path.split('/')[1:]
         print(pth)
         if(pth[0] == 'wifi'):
             self.wfile.write(str.encode("%s" %names))
+            if(pth[1] == 'connect'):
+                save_wifi(self.path)
         else:
             self.wfile.write(b'You Have Reached The Pi, Congrats!')
     def do_HEAD(self):
